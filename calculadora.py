14ead6528b4c1cfd809af7019d2eeba33f4d131a
lvcas-dev <lvcasout@gmail.com>
2026-05-06 18:53:45 -0300

    feat: calculadora com quatro operações

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+operador = input("Operador Aritimético: ")
+primeiroNumero = int(input("Digite um número: "))
+segundoNumero = int(input("Digite outro número: "))
+
+if operador == '+'
+    print(primeiroNumero + segundoNumero)
+elif operador == '-' 
+    print(primeiroNumero - segundoNumero) 
+elif operador == '*' 
+    print(primeiroNumero * segundoNumero)
+else 
+    print(primeiroNumero / segundoNumero)
+
